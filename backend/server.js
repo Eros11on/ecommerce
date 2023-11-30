cad5e490e3c97e7c668a57924d21e3371e864b32
qiqi47 <c756738098@126.com>
2023-11-30 14:07:26 -0600

    backend

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+connectDB();
+const app = express();
+const PORT = process.env.port || 5001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
